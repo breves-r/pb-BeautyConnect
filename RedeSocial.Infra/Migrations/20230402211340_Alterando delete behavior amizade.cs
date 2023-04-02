37af7655a1d0ff10f51f8d1492987be2099187bc
Maria Eduarda MAFRA <mariaszmafra@gmail.com>
2023-04-02 18:49:48 -0300

    Alteração view profile details

---
+++ RedeSocial.Infra/Migrations/20230402211340_Alterando delete behavior amizade.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RedeSocial.Infra.Migrations
+{
+    public partial class Alterandodeletebehavioramizade : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Friendships_Profiles_IdProfileA",
+                table: "Friendships");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Friendships_Profiles_IdProfileB",
+                table: "Friendships");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Friendships_Profiles_IdProfileA",
+                table: "Friendships",
+                column: "IdProfileA",
+                principalTable: "Profiles",
+                principalColumn: "IdProfile",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Friendships_Profiles_IdProfileB",
+                table: "Friendships",
+                column: "IdProfileB",
+                principalTable: "Profiles",
+                principalColumn: "IdProfile",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Friendships_Profiles_IdProfileA",
+                table: "Friendships");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Friendships_Profiles_IdProfileB",
+                table: "Friendships");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Friendships_Profiles_IdProfileA",
+                table: "Friendships",
+                column: "IdProfileA",
+                principalTable: "Profiles",
+                principalColumn: "IdProfile",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Friendships_Profiles_IdProfileB",
+                table: "Friendships",
+                column: "IdProfileB",
+                principalTable: "Profiles",
+                principalColumn: "IdProfile",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
